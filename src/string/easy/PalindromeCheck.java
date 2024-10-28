4768c4661bd6300f600d0ff12c8ac3c0fa417615
himal <himal.rg@gmail.com>
2024-10-28 17:02:04 +0000

    added solution for caesar cipher encryptor problem

--- src/string/easy/PalindromeCheck.java
+++ src/string/easy/PalindromeCheck.java
@@ -6,7 +6,7 @@ class PalindromeCheck {
         // o(n) sc
         StringBuilder s = new StringBuilder();
         // O(n) tc
-        for(int i = str.length() - 1; i >=0; i--){
+        for (int i = str.length() - 1; i >= 0; i--) {
             s.append(str.charAt(i));
         }
         // O(n) tc
@@ -17,8 +17,8 @@ class PalindromeCheck {
     public static boolean isPalindrome1(String str) {
         int startIndex = 0;
         int endIndex = str.length() - 1;
-        while(startIndex < endIndex){
-            if((str.charAt(startIndex) != str.charAt(endIndex))){
+        while (startIndex < endIndex) {
+            if ((str.charAt(startIndex) != str.charAt(endIndex))) {
                 return false;
             }
             startIndex++;
