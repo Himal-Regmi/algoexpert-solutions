eec42d10fad1a37f6243a1063a87d72c84200d91
himal <himal.rg@gmail.com>
2025-05-19 12:12:34 +0100

    added solution for binary trees easy - evaluate expression tree

---
+++ src/main/java/binarytrees/easy/EvaluateExpressionTree.java
@@ -0,0 +1,29 @@
+package binarytrees.easy;
+
+//O(n) and O(h)
+public class EvaluateExpressionTree {
+  // This is an input class. Do not edit.
+  static class BinaryTree {
+    public int value;
+    public BinaryTree left = null;
+    public BinaryTree right = null;
+
+    public BinaryTree(int value) {
+      this.value = value;
+    }
+  }
+
+  public int evaluateExpressionTree(BinaryTree tree) {
+    if (tree.value == -1) {
+      return evaluateExpressionTree(tree.left) + evaluateExpressionTree(tree.right);
+    } else if (tree.value == -2) {
+      return evaluateExpressionTree(tree.left) - evaluateExpressionTree(tree.right);
+    } else if (tree.value == -3) {
+      return evaluateExpressionTree(tree.left) / evaluateExpressionTree(tree.right);
+    } else if (tree.value == -4) {
+      return evaluateExpressionTree(tree.left) * evaluateExpressionTree(tree.right);
+    } else {
+      return tree.value;
+    }
+  }
+}
