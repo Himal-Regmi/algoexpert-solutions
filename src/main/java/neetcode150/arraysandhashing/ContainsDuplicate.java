4e501c1022ab7c0dba7e3bfa0371b60c9d1e49fd
himal <himal.rg@gmail.com>
2025-12-02 23:59:32 +0000

    added solution for needcode150 - arrays and hashing - contains duplicate

--- src/main/java/neetcode150/arraysandhashing/ContainsDuplicate.java
+++ src/main/java/neetcode150/arraysandhashing/ContainsDuplicate.java
@@ -3,14 +3,15 @@ package neetcode150.arraysandhashing;
 import java.util.HashSet;
 
 public class ContainsDuplicate {
+  // O(n) & O(n)
   class Solution {
     public boolean hasDuplicate(int[] nums) {
       HashSet<Integer> visitedNumbers = new HashSet<>();
-      for (int i = 0; i < nums.length; i++) {
-        if (visitedNumbers.contains(nums[i])) {
+      for (int num : nums) {
+        if (visitedNumbers.contains(num)) {
           return true;
         }
-        visitedNumbers.add(nums[i]);
+        visitedNumbers.add(num);
       }
       return false;
     }
