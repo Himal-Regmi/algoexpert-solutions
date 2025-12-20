2f45b669690dfa99d9b03ce4f1321ebe694f2e60
himal <himal.rg@gmail.com>
2025-12-20 23:32:55 +0000

    added solution for needcode150 - sliding window - is palindrome

---
+++ src/main/java/neetcode150/slidingwindow/IsPalindrome.java
@@ -0,0 +1,29 @@
+package neetcode150.slidingwindow;
+
+public class IsPalindrome {
+  class Solution {
+    public boolean isPalindrome(String s) {
+      int leftIdx = 0;
+      int rightIdx = s.length() - 1;
+
+      while (leftIdx < rightIdx) {
+        while (leftIdx < rightIdx && !isAlphaNumeric(s.charAt(leftIdx))) {
+          leftIdx++;
+        }
+        while (leftIdx < rightIdx && !isAlphaNumeric(s.charAt(rightIdx))) {
+          rightIdx--;
+        }
+        if (Character.toLowerCase(s.charAt(leftIdx)) != Character.toLowerCase(s.charAt(rightIdx))) {
+          return false;
+        }
+        leftIdx++;
+        rightIdx--;
+      }
+      return true;
+    }
+
+    public boolean isAlphaNumeric(char c) {
+      return (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' || c >= '0' && c <= '9');
+    }
+  }
+}
