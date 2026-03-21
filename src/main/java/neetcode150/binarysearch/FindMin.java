0cee855ba3a5750d3e92245b5f5c2d7a4a5f6eb3
himal <himal.rg@gmail.com>
2026-03-21 00:10:16 +0000

    added optimal solution for needcode150 - binary search - minimum number in rotated sorted array

---
+++ src/main/java/neetcode150/binarysearch/FindMin.java
@@ -0,0 +1,27 @@
+package neetcode150.binarysearch;
+
+public class FindMin {
+  // O(logn) && O(1)
+  class Solution {
+    public int findMin(int[] nums) {
+      int l = 0;
+      int r = nums.length - 1;
+      int result = nums[0];
+
+      while (l <= r) {
+        if (nums[l] < nums[r]) {
+          result = Math.min(nums[l], result);
+          break;
+        }
+        int mid = l + (r - l) / 2;
+        result = Math.min(nums[mid], result);
+        if (nums[l] > nums[mid]) {
+          r = mid - 1;
+        } else {
+          l = mid + 1;
+        }
+      }
+      return result;
+    }
+  }
+}
